8b889d473b7b91ac57709afedefa0d2e7bee7fca
nicsan6 <nictrabalhos@gmail.com>
2026-05-12 07:26:59 -0300

    atividade 01 atualizada

--- Aula07/atividade01.py
+++ Aula07/atividade01.py
@@ -1,116 +1,148 @@
 '''
-    Desenvolva um sistema de gerenciamento de veículos, permita cadastrar o veículo pagando do usuário os seguintes dados (modelo, marca, preço)
 
-        - os dados devem ser armazenados em um arquivo.
-        - o usuário deve poder cadastrar quantos carros quiser sem ter que rodar o sistema novamente.
-        - deve ter a opção de ler os carros existentes
-        - devem ser cadastrados em um arquivo .txt e usar dicionário
+Desenvolva um sistema de gerenciamento de veiculos, permita cadastrar o veículo pegando do usuário os seguintes dados (modelo, marca, preço)
+    - Os dados devem ser armazenados em um arquivo.
+    - o usuário deve poder cadastrar quantos carros quiser sem ter que rodar o sistema novamente.
+    - deve ter a opção de ler os carros existentes
+    - devem ser cadastrados eu um arquivo .txt e usar dicionário.
+
 '''
-import os
 import time
-carros = []
-proximo_id = 1
+import os
+carros=[]
+proximo_id=1
 
-os.system('cls')
+open("carros.txt", "a").close()
+
+with open("carros.txt", "r", encoding="utf-8") as arquivo:
+    for linha in arquivo:
+
+        dados=linha.strip().split(";")
+
+        if len(dados)==4:
+
+            carro ={
+                "id": int(dados[0]),
+                "modelo": dados[1],
+                "preço": dados[2],
+                "marca": dados[3]
+            }
+
+            carros.append(carro)
+
+            if carro["id"] >= proximo_id:
+                proximo_id = carro["id"] + 1
+
+os.system("cls")
 while True:
-    print("\n===== Sistema de Carros 🚗 =====")
-    print('1. - Cadastrar carro')
-    print('2. - Listar carros')
-    print('3. - Atualizar carro')
-    print('4. - Deletar carro')
-    
-    opcao = input("Escolha uma opção: ")
-    
-    #criar
-    if opcao == '1':
-        modelo = input("Digite o modelo do carro: ").title()
-        preco = input('Digite o preço do carro: ')
-        marca = input("Digite a marca: ").title()
+    print("\n----------------- Sistema de carros 🚗 -----------------")
+    print('1. cadastrar carros')
+    print('2. listar carros')
+    print('3. Atualizar carros')
+    print('4. deletar carros')
+    print('0. sair')
 
-        carro = {
-            "id": proximo_id,
-            "modelo": modelo,
-            "preço": preco,
-            "marca": marca
-        }
+    opcao=input("escolha uma das opções: ")
 
-    with open('carros.txt', 'w') as arquivo:
-        arquivo.write(f'ID: {carro['id']} | Modelo: {carro['modelo']} | Preço: {carro['preço']} | Marca: {carro['marca']}')
-       
+    #create
+    if opcao=="1":
+        os.system("cls")
+        while True:
+            modelo = input("Digite o modelo do carro: ").title()
+            preco = input('Digite o preço do carro: ').replace(',','.')
+            marca = input("Digite a marca: ").title()
 
-        proximo_id += 1
+            carro ={
+                "id": proximo_id,
+                "modelo": modelo,
+                "preço": preco,
+                "marca": marca
 
-        print("✅Carro cadastro com sucesso!")
-        
+            }
+            
+            carros.append(carro)
+            proximo_id +=1
+            print('✅ Carro cadastrado')
+            sair = input("Deseja adicionar mais? (s - sim) ou enter para parar!").lower()
+            if sair !="s":
+                break
 
-
-
-
-#read
-    if opcao == '2':
+    #read
+    elif opcao =="2":
+        os.system("cls")
         if not carros:
-            print('⚠️Nenhum carro cadastrado.')
+            print('⚠️ Nenhum carro encontrado')
         else:
-            print('\n📋Lista de carros')
+            print('\n📋 Lista de carros')
+            for carro in carros:
+                print(f'ID: {carro['id']} | Modelo: {carro['modelo']} | Preço: {carro['preço']} | Marca: {carro['marca']}')
+
+    #update
+    elif opcao =='3':
+        os.system("cls")
+        print('\n📋 Lista de carros')
         for carro in carros:
             print(f'ID: {carro['id']} | Modelo: {carro['modelo']} | Preço: {carro['preço']} | Marca: {carro['marca']}')
-    #update 
-    elif opcao == '3':
-        print('\n📋Lista de carros')
+        id_busca = int(input("Digite o id do carro para deletar: "))
+
+        encontrado=False
+        for carro in carros:
+            if carro['id']==id_busca:
+                novo_modelo = input('Digite o novo modelo: ').title()
+                novo_preco = input('Digite o novo preço: ').replace(',','.')
+                nova_marca = input('Digite a nova marca: ').title()
+
+                carro["modelo"] = novo_modelo
+                carro["preço"] = novo_preco
+                carro["marca"] = nova_marca
+
+                print("✅ Carro encontrado com sucesso! ")
+                encontrado = True
+                break
+        if not encontrado:
+            print("❌ Carro não encontrado!")
+                
+    #delete
+    elif opcao =='4':
+        os.system("cls")
+        print('\n📋 Lista de carros')
         for carro in carros:
             print(f'ID: {carro['id']} | Modelo: {carro['modelo']} | Preço: {carro['preço']} | Marca: {carro['marca']}')
-        id_busca = int(input("Digite o ID do carro para editar: "))
+        id_busca = int(input("Digite o id do carro para deletar: "))
+       
+        encontrado=False
 
-        encontrado = False
         for carro in carros:
-            if carro ['id'] == id_busca:
-                novo_modelo = input("Digite o novo modelo: ").title()
-                novo_preco = input("Digite o novo preço: ").replace(',', '.')
-                novo_marca = input("Digite a nova marca: ").title()
-
-                carro['modelo'] = novo_modelo
-                carro['preco'] = novo_preco
-                carro['marca'] = novo_marca
-
-                with open('carros.txt', 'w') as arquivo:
-                    arquivo.write(f'Modelo {novo_modelo['modelo']} | Preço {novo_preco['preco']} | Marca: {novo_marca['marca']}')
-
-                print('✅Carro atualizado com sucesso!')
-                econtrado = True
+            if carro['id']==id_busca:
+                carros.remove(carro)
+                print("✅ Carro deletado com sucesso")
+                encontrado=True
                 break
 
         if not encontrado:
-            print("❌O carro não foi encontrado!")
+            print("❌ Carro não encontrado!")
 
-    #delete
-    elif opcao == '4':
-        print('\n📋Lista de carros')
-        for carro in carros:
-            print(f'ID: {carro['id']} | Modelo: {carro['modelo']} | Preço: {carro['preço']} | Marca: {carro['marca']}')
-        id_busca = int(input("Digite o ID do carro para deletar: "))
-
-        #variável de controle (não precisa fazer um else n o acesso da lista)
-        econtrado = False
-
-        #acessando e percorrendo a lista para verificar 
-        for carro in carros:
-            if carro ['id'] == id_busca:
-                carros.remove(carro)
-                print('✅Carro deletado com sucesso!')
-                econtrado = True
-                break
-        if not econtrado:
-            print("❌O carro não foi encontrado!")
     #sair
     elif opcao == '0':
-        total = 10
-        barra = ""
-        print('Saindo do sistema...')
-        for i in range(total + 1):
-            barra += "🟩"
-            porcentagem = int((i / total *100)) 
-            vazio = "-" * (total-1)
-            print(f'\r[{barra}] {porcentagem}%')
-        time.sleep(0.3)
+        with open("carros.txt", "w", encoding="utf-8") as arquivo:
+            for carro in carros:
+                linha = (
+                    f"{carro['id']};"
+                    f"{carro['modelo']};"
+                    f"{carro['preço']};"
+                    f"{carro['marca']}\n"
+                )
+                arquivo.write(linha)
+        total = 20
+        barra =""
+        print('Saindo do Sistema...')
+        for i in range(1, total +1):
+            barra +="🟩"
+            porcentagem = int((i / total *100))
+            vazio = "-" * (total -1)
+            print(f'\r[{barra}] {porcentagem}%', end="")
+            time.sleep(0.2)
         break
-    else: print('❌Opção inválida.')
+
+    else:
+        print('❌ Opção inválida.')
